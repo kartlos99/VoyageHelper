2c293b27a026c29c86ee71f55be359d88ca88961
kartlos diakonidze <kartlos99>
2015-07-20 00:24:45 +0400

    პატარა ჩასწორება განცხადების დამატების ფორმაში

--- Voiage1/app/src/main/java/diakonidze/kartlos/voiage/fragments/AddDriverStatementF.java
+++ Voiage1/app/src/main/java/diakonidze/kartlos/voiage/fragments/AddDriverStatementF.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import diakonidze.kartlos.voiage.R;
@@ -23,8 +26,26 @@ public class AddDriverStatementF extends Fragment {
         Spinner cityTo = (Spinner) view.findViewById(R.id.driver_cityto);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.cityes, android.R.layout.simple_spinner_item);
-
         cityFrom.setAdapter(adapter);
+
+        CheckBox pirobebi = (CheckBox) view.findViewById(R.id.driver_comfort_checkBox);
+        final LinearLayout comfort1 = (LinearLayout) view.findViewById(R.id.comfort1);
+        final LinearLayout comfort2 = (LinearLayout) view.findViewById(R.id.comfort2);
+
+
+        pirobebi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    comfort1.setVisibility(View.VISIBLE);
+                    comfort2.setVisibility(View.VISIBLE);
+                }else{
+                    comfort1.setVisibility(View.GONE);
+                    comfort2.setVisibility(View.GONE);
+                }
+            }
+        });
+
 
         return view;
     }
