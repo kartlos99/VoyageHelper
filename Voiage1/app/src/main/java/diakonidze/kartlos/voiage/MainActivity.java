3d0e30b71a1f85638d7e7425fb5030f25aec2439
kartlos99 <kartlos99>
2015-07-23 16:27:53 +0400

    დაემატა მენიუ NavigationView -თი  issue #17

--- Voiage1/app/src/main/java/diakonidze/kartlos/voiage/MainActivity.java
+++ Voiage1/app/src/main/java/diakonidze/kartlos/voiage/MainActivity.java
@@ -3,11 +3,17 @@ package diakonidze.kartlos.voiage; //1.0.3
 import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -27,18 +33,68 @@ import java.util.Calendar;
 import diakonidze.kartlos.voiage.models.DriverStatement;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     public final Context mainctx = this;
-
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
 
     ViewPager pager = null;
-    ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.manuFilter:
+                        Toast.makeText(getApplicationContext(),"filtri",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.manuExit:
+                        Toast.makeText(getApplicationContext(),"exit",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.openDrawer, R.string.closeDrawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Toast.makeText(getApplicationContext(),"daiketa",Toast.LENGTH_SHORT).show();
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Toast.makeText(getApplicationContext(),"gaigo",Toast.LENGTH_SHORT).show();
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        //calling sync state is necessay or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
 
         pager = (ViewPager) findViewById(R.id.pageView);
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
---
+++ Voiage1/app/src/main/java/diakonidze/kartlos/voiage/forTabs/SlidingTabLayout.java
@@ -0,0 +1,7 @@
+package diakonidze.kartlos.voiage.forTabs;
+
+/**
+ * Created by k.diakonidze on 7/23/2015.
+ */
+public class SlidingTabLayout {
+}
